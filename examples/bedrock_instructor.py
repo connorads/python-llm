cceca4ffb2dc5bccd4c4c9bc2268c01175aa7335
Connor Adams <connorads@users.noreply.github.com>
2024-06-01 16:15:25 +0100

    Use Joke structured example

--- examples/bedrock_instructor.py
+++ examples/bedrock_instructor.py
@@ -1,15 +1,14 @@
 import instructor
 from anthropic import AnthropicBedrock
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from structlog import get_logger
 
 logger = get_logger()
 
 
-class User(BaseModel):
-    name: str
-    age: int
-
+class Joke(BaseModel):
+    setup: str = Field(description="The setup of the joke")
+    punchline: str = Field(description="The punchline to the joke")
 
 client = instructor.from_anthropic(AnthropicBedrock(aws_region="us-east-1"))
 
@@ -20,14 +19,12 @@ resp = client.messages.create(
     messages=[
         {
             "role": "user",
-            "content": "Extract Jason is 25 years old.",
+            "content": "Tell me a joke about fintech lending to UK SMEs",
         }
     ],
-    response_model=User,
+    response_model=Joke,
 )
 
-assert isinstance(resp, User)
-assert resp.name == "Jason"
-assert resp.age == 25
+assert isinstance(resp, Joke)
 
 logger.info("LLM Made prediction", content=resp.model_dump_json())
